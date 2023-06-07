59077d0b4ecec622ca0384af6cca0ee85ebd6af1
asraf76 <gustomi18012000@gmail.com>
2023-06-07 20:36:03 +0700

    notescard

--- lib/screen/notes_card.dart
+++ lib/screen/notes_card.dart
@@ -1,33 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list/Models/notes_operation.dart';
 import 'package:todo_list/Models/note.dart';
+import 'package:todo_list/screen/add_screen.dart';
+import 'package:todo_list/screen/edit_screen.dart';
+import 'package:todo_list/screen/notes_card.dart';
 
 class NotesCard extends StatelessWidget {
   final Note note;
-  const NotesCard({super.key, required this.note});
+
+  const NotesCard({required this.note});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15),
-      padding: const EdgeInsets.all(15),
-      height: 150,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            note.title,
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 5),
-          Text(
-            note.description,
-            style: const TextStyle(fontSize: 17),
-          ),
-        ],
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+      child: ListTile(
+        title: Text(
+          note.title,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(note.description),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EditScreen(note: note),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.edit, color: Colors.blueGrey),
+            ),
+            IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Confirm Deletion'),
+                      content: const Text('Are you sure you want to delete this note?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            // Perform delete operation
+                            Provider.of<NotesOperation>(context, listen: false).deleteNote(note);
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Delete'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Cancel'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              icon: const Icon(Icons.delete, color: Colors.red),
+            ),
+          ],
+        ),
       ),
     );
   }
